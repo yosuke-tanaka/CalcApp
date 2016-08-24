11b2519c13e499a5ead7a44709e751ad6db81255
yosuke-tanaka <y_tanaka_ym@yahoo.co.jp>
2016-08-24 21:24:14 +0900

    以下2点修正： (1) EditText が空欄のまま計算用ボタンを押すとアプリが落ちる。 (2) EditText に数値以外の文字を入力して計算ボタンを押すとアプリが落ちる。

--- app/src/main/java/com/example/yt_home/calcapp/MainActivity.java
+++ app/src/main/java/com/example/yt_home/calcapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.yt_home.calcapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mEditText1 = (EditText) findViewById(R.id.editText1);
         mEditText2 = (EditText) findViewById(R.id.editText2);
+        // 2016.08.24 [修正] EditTextの入力を数値に制限
+        mEditText1.setInputType( InputType.TYPE_CLASS_NUMBER);
+        mEditText2.setInputType( InputType.TYPE_CLASS_NUMBER);
 
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int calcId = 0;  // 演算種別
+        double val1;
+        double val2;
 
         if (v.getId() == R.id.button1) {
             calcId = 0;
@@ -44,9 +51,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             calcId = 3;
         }
 
+        Log.d("ANDROID", "--------------------------"); // デバッグ用に追加する
+
+        // 2016.08.24 [修正] 空文字列チェック
+        if(mEditText1.getText().toString().equals(""))
+        {
+            Log.d("ANDROID", "mEditText1は空文字列"); // デバッグ用に追加する
+            return;
+        }
+        if(mEditText2.getText().toString().equals(""))
+        {
+            Log.d("ANDROID", "mEditText2は空文字列"); // デバッグ用に追加する
+            return;
+        }
+        Log.d("ANDROID", "mEditText1,2は空文字列ではない"); // デバッグ用に追加する
+
+        val1 = Double.parseDouble(mEditText1.getText().toString());
+        val2 = Double.parseDouble(mEditText2.getText().toString());
+
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("VALUE1", Double.parseDouble(mEditText1.getText().toString()));
-        intent.putExtra("VALUE2", Double.parseDouble(mEditText2.getText().toString()));
+        intent.putExtra("VALUE1", val1);
+        intent.putExtra("VALUE2", val2);
         intent.putExtra("VALUE3", calcId);
         startActivity(intent);
     }
